dbb2a4fe7e8df923d43e6f3160e740ef455d9ac2
dingsai88 <dingsai88@126.com>
2020-02-22 00:43:54 +0800

    spring全家桶

---
+++ src/main/java/com/ding/spring/spring2020study/Spring2020StudyApplication.java
@@ -0,0 +1,13 @@
+package com.ding.spring.spring2020study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2020StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2020StudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ding/spring/spring2020study/Spring2020StudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ding.spring.spring2020study;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring2020StudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
