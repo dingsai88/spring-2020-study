df767058899e2a97271820aa07915003c4fb4a99
dingsai88 <dingsai88@126.com>
2020-03-13 02:10:37 +0800

    dev_06如何配置多数据源

--- src/main/java/com/ding/spring/spring2020study/Spring2020StudyApplication.java
+++ src/main/java/com/ding/spring/spring2020study/Spring2020StudyApplication.java
@@ -4,21 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.SQLException;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, JdbcTemplateAutoConfiguration.class})
 public class Spring2020StudyApplication implements CommandLineRunner {
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Spring2020StudyApplication.class);
-    @Autowired
-    private DataSource dataSource;
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(Spring2020StudyApplication.class, args);
@@ -26,21 +31,62 @@ public class Spring2020StudyApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        showData();
-        showConnection();
+        log.info("run-000000000");
 
     }
 
-    private void showConnection() throws SQLException {
-        log.info("dataSource1:"+dataSource.toString());
-        Connection conn = dataSource.getConnection();
-        log.info("conn1："+conn.toString());
-        conn.close();
+    @Primary
+    @Bean
+    @ConfigurationProperties("foo.datasource")
+    public DataSourceProperties getFooDataSourceProperties() {
+        return new DataSourceProperties();
     }
 
-    private void showData() {
-        log.info("showData11：");
-        jdbcTemplate.queryForList("SELECT * FROM FOO")
-                .forEach(row -> log.info("1111:"+row.toString()));
+    @Bean
+    public DataSource fooDataSource() {
+        DataSourceProperties dataSourceProperties = getFooDataSourceProperties();
+        log.info("getFooDataSource.url:{}", dataSourceProperties.getUrl());
+        return dataSourceProperties.initializeDataSourceBuilder().build();
     }
+
+    @Bean
+    @Resource
+    public PlatformTransactionManager fooManager(DataSource fooDataSource) {
+        try {
+            log.info("fooManager.PlatformTransactionManager.url:{}", fooDataSource.getConnection().getMetaData().getURL());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new DataSourceTransactionManager(fooDataSource);
+    }
+//////////////////////////////////////////
+
+    @Bean
+    @ConfigurationProperties("bar.datasource")
+    public DataSourceProperties barDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource barDataSource() {
+        DataSourceProperties dataSourceProperties = barDataSourceProperties();
+        log.info("bar datasource: {}", dataSourceProperties.getUrl());
+        return dataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @Resource
+    public PlatformTransactionManager barTxManager(DataSource barDataSource) {
+        try {
+            log.info("barTxManager.PlatformTransactionManager.url:{}", barDataSource.getConnection().getMetaData().getURL());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return new DataSourceTransactionManager(barDataSource);
+    }
+
 }
